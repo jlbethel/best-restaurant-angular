c8a49f4cb2c4211646354adabe70961c750bc75d
jose torres & Jason Bethel <student@epicodus.com>
2015-09-21 12:27:22 -0700

    change location and price inputs to selectors

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,10 +1,15 @@
 restaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
-$scope.restaurants=[];
-$scope.addRestaurant = function() {
-  $scope.restaurants.push({name: $scope.restaurantName, food: $scope.restaurantFood, location: $scope.restaurantLocation, price: $scope.restaurantPrice});
-  $scope.restaurantName = null;
-  $scope.restaurantFood = null;
-  $scope.restaurantLocation = null;
-  $scope.restaurantPrice = null;
-};
+  $scope.restaurants=[];
+  $scope.addRestaurant = function() {
+    $scope.restaurants.push({name: $scope.restaurantName, food: $scope.restaurantFood, location: $scope.restaurantLocation, price: $scope.restaurantPrice});
+    $scope.restaurantName = null;
+    $scope.restaurantFood = null;
+    $scope.restaurantLocation = null;
+    $scope.restaurantPrice = null;
+  };
+
+  $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.restaurants.indexOf(restaurant);
+    $scope.restaurants.splice(index, 1);
+  };
 });
